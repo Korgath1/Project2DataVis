139ad3bab45cd4b74ddcbce9ed1cb2d2100929f9
Korgath1 <santosadducci@gmail.com>
2025-11-18 01:29:19 -0700

    new changes(?)

--- streamlit_CS/pages/Future_Works.py
+++ streamlit_CS/pages/Future_Works.py
@@ -4,18 +4,22 @@ st.title("Future Work & Reflection")
 
 st.header("Next Steps")
 
+st.header("Next Steps")
 st.markdown("""
-Potential improvements;
+Potential improvements:
 1. Live updates on the dashboards using an API
-2. Page to upload CSVs with the same collumn types
-3. More data, traffic volume, world region, or even past records on drivers. It would be nice to know if the driver has gotten a DUI before or other tickets
-            """
+2. Page to upload CSVs with the same column types
+3. More data: traffic volume, world region, or even past records on drivers like DUIs
+""")
 
 st.header("Reflection on Project Evolution")
 st.markdown("""
-From Lab 4.3 I changed the data, I wanted something different on traffic. I thought the dataset with music was not as fun as it could be.
-Music for me is constantly changing and it was not as fun working with a dataset that was so old. Here, traffic data, stays pretty similar
-from year to year. This project has a gallery, a dashboard, I even added a selected insights page that answers the rubric directly. It was refreshing
-to work with new data. I learned from previous projects how to set up a dynamic path to the data which I had a hard time troubleshooting last time.
-This project I think is much more interactive and you can really cut at the data from any direction unlike the work on the last lab.
-            """
+From Lab 4.3, I changed the data because I wanted something different on traffic. 
+I thought the previous dataset with music was not as fun as it could be. 
+Music is constantly changing, and it was not as fun working with a dataset that was so old. 
+Comparatively traffic data stays relatively similar from year to year.
+
+This project now includes a gallery, a dashboard, and a selected insights page that answers the rubric directly. 
+I learned from previous projects how to set up a dynamic path to the data, which I had troubleshooting with last time. 
+This project is much more interactive, and you can really cut at the data from any direction unlike the work on the last lab.
+""")
