c7775009c364d37b73c207efb6a7d5c7f03ee9bf
Korgath1 <santosadducci@gmail.com>
2025-11-18 01:19:38 -0700

    future works page

---
+++ streamlit_CS/pages/Future_Works.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+st.title("Future Work & Reflection")
+
+st.header("Next Steps")
+
+st.markdown("""
+Potential improvements;
+1. Live updates on the dashboards using an API
+2. Page to upload CSVs with the same collumn types
+3. More data, traffic volume, world region, or even past records on drivers. It would be nice to know if the driver has gotten a DUI before or other tickets
+            """
+
+st.header("Reflection on Project Evolution")
+st.markdown("""
+From Lab 4.3 I changed the data, I wanted something different on traffic. I thought the dataset with music was not as fun as it could be.
+Music for me is constantly changing and it was not as fun working with a dataset that was so old. Here, traffic data, stays pretty similar
+from year to year. This project has a gallery, a dashboard, I even added a selected insights page that answers the rubric directly. It was refreshing
+to work with new data. I learned from previous projects how to set up a dynamic path to the data which I had a hard time troubleshooting last time.
+This project I think is much more interactive and you can really cut at the data from any direction unlike the work on the last lab.
+            """
